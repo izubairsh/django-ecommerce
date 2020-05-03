cd928a1720b4a16a6151570e707eb8f32531b649
Zubair Sheikh <zubair.sh5995@outlook.com>
2020-05-03 19:47:02 +0500

    store, cart & checkout pages

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
