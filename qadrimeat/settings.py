93ed643c0738ae39e4f9465bdd62974990167ccb
Zubair Sheikh <zubair.sh5995@outlook.com>
2020-06-26 15:22:04 +0500

    Added local settings

--- qadrimeat/settings.py
+++ qadrimeat/settings.py
@@ -145,3 +145,8 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
